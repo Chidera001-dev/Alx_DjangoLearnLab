74f53a215a368dedccf440181a477cd2bda6a1d5
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-09-06 17:38:42 +0100

    remove delete book route from urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
 
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
 
 
