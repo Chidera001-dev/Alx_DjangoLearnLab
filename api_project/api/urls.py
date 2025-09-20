edd8228ffdd2d0c978cc858636ce8abf08d4d7b0
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-09-20 21:55:44 +0100

    added another route and a CRUD functionality

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -10,4 +10,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Include the router URLs for BookViewSet (all CRUD operations)
+    path('', include(router.urls)),  # This includes all routes registered with the router
 ]
