f5445064518e34855869fd87fbe4d22e0ff22a33
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-09-27 16:59:44 +0100

    feat: implement CRUD views and URL routing for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import generics
+
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookListDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 # Create your views here.
