6e3be7d89b03d7517e9c87a48493bca8b825f397
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-10-11 15:03:48 +0100

    added my route for like and unlike views

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('', include(router.urls)),
 
     path('feed/', FeedView.as_view(), name='feed'),
-    path('like/<int:post_id>/', LikePostView.as_view(), name='like_post'),
-    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
-
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
